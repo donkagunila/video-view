8805ddb3fc11ebe7150892912cf069a2a298c73a
Donald Kagunila <donkagunila@gmail.com>
2018-10-23 06:55:01 +0300

    add newslatter

--- js/app.js
+++ js/app.js
@@ -8,5 +8,31 @@ $(document).ready(function () {
 	}),overlap.on('click', function () {
 		$(this).hide();
 		$(document.body).removeClass('has-overlap'),sidenav.hide();
-	})
+	});
+
+
+	// scroll reveal
+	// window.sr = new scrollReveal();
+	ScrollReveal().reveal('.left-in', {
+		duration: 2000,
+		origin: 'left',
+		distance: '30px',
+		reset: true
+	});
+
+	ScrollReveal().reveal('.bottom-in', {
+		duration: 2000,
+		origin: 'bottom',
+		distance: '10px',
+		reset: true
+	});
+
+	ScrollReveal().reveal('.right-in', {
+		duration: 2000,
+		origin: 'right',
+		distance: '30px',
+		reset: true
+	});
+	// sr.reveal('.right-in');
+
 });
