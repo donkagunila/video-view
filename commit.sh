ad43c7bb60b1a6c8f41b4224c3f8898879fab9cc
Donald Kagunila <donkagunila@gmail.com>
2018-11-06 20:53:48 +0300

    [A

--- commit.sh
+++ commit.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
+echo Status of the files
+git status
+echo addin
 git add .
-
+echo please enter message
 read varmessage
+echo git commit -m "$varmessage"
 git commit -m "$varmessage"
+echo git push
 git push
